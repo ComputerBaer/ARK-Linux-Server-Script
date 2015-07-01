fc1f8bc96023d538bc4ab261c2134275e15a9eec
Brandon <zendrexgaming@gmail.com>
2015-07-01 01:51:00 -0700

    Update arkserver.sh

--- arkserver.sh
+++ arkserver.sh
@@ -11,7 +11,7 @@ RESET='\e[0m'
 ERR='\e[1;31m ERROR\e[0m'
 
 # Version Checker
-version="1.1.0"
+version="1.1.1"
 
 echo -e; echo -e "$YELLOW Checking version with github. $RESET"
 if [ -f version.ini ]; then
