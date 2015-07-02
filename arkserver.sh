e778c88922f10a98e798f832441ed257184645eb
Brandon <zendrexgaming@gmail.com>
2015-07-01 18:06:24 -0700

    Update arkserver.sh

--- arkserver.sh
+++ arkserver.sh
@@ -196,6 +196,11 @@ if [ ! -f backupserver ]; then
     curl https://raw.githubusercontent.com/Zendrex/ARK-Linux-Server-Script/master/.serverscript/backupserver -o backupserver -#
     chmod 777 backupserver
 fi
+# Server Status Script
+if [ ! -f serverstatus ]; then
+    echo -e "$YELLOW Server Status Script $RESET"
+    curl https://raw.githubusercontent.com/Zendrex/ARK-Linux-Server-Script/master/.serverscript/serverstatus -o serverstatus -#
+fi
 # Formatting File
 if [ ! -f formatting.ini ]; then
     echo -e "$YELLOW Formatting $RESET"
@@ -211,7 +216,7 @@ fi
 # Commands
 help () {
     echo -e; echo -e "$WHITE Use the following commands: $RESET"
-    echo -e; echo -e "$CYAN ./arkserver.sh <start|stop|view|install|update|backup> $RESET"
+    echo -e; echo -e "$CYAN ./arkserver.sh <start|stop|view|status|install|update|backup> $RESET"
 	echo -e; echo -e
 }
 
@@ -243,6 +248,11 @@ update () {
 backup () {
     clear
     ./backupserver
+}
+
+status () {
+	clear
+	./serverstatus
 }
 
 [ "$1" = "" ] && {
